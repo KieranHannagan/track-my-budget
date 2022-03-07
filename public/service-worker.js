0e2611f1e25827563e242115e9dbc95ede4dca5e
Sam Vascellaro <samvascellaro@Sams-MacBook-Pro.local>
2022-03-06 23:14:27 -0600

    fixed manifest and cache error

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,10 +6,12 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./js/idb.js",
     "./js/index.js",
-    "./assets/css/style.css",
+    "./css/styles.css",
 ];
 
 
+
+// Cache resources
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -19,26 +21,28 @@ self.addEventListener('install', function (e) {
     )
 })
 
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
             let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
-            });
+            })
+            // add current cache name to keeplist
             cacheKeeplist.push(CACHE_NAME);
 
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
         })
     );
 });
 
+
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
@@ -50,9 +54,6 @@ self.addEventListener('fetch', function (e) {
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
             }
-
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
         })
     )
 })
